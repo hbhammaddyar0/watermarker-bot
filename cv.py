fb47fbe2c69e7e5b0429d25a327106606f5522ce
HB Hammad Dyar <66479406+hbhammaddyar0@users.noreply.github.com>
2021-06-17 09:38:46 +0500

    Update cv.py

--- cv.py
+++ cv.py
@@ -1,7 +1,7 @@
 # Here you can place your constant variables
 # Like chat_ids, quotes, buttons, ...
 
-Id_Admins = [1160089400,]
+Id_Admins = [1853610435:AAGrrCzsK8Whcr4EenHM8ktUo8udLPZsk_w,]
 
 LOGOS = ['KDramas Urdu2.png']
 
